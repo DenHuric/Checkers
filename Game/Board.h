aaa1cdba3499be2543f270bb39b4d2784096b8c5
dkhurich <dhurich231@gmail.com>
2025-03-05 17:09:45 +0300

    Финальный вариант проекта

--- Game/Board.h
+++ Game/Board.h
@@ -7,8 +7,8 @@
 #include "../Models/Project_path.h"
 
 #ifdef __APPLE__
-    #include <SDL2/SDL.h>
-    #include <SDL2/SDL_image.h>
+    #include <SDL.h>
+    #include <SDL_image.h>
 #else
     #include <SDL.h>
     #include <SDL_image.h>
@@ -19,43 +19,56 @@ using namespace std;
 class Board
 {
 public:
-    Board() = default;
+    Board() = default; // Конструктор по умолчанию
+
+    // Конструктор, инициализирующий ширину (W) и высоту (H) доски
     Board(const unsigned int W, const unsigned int H) : W(W), H(H)
     {
     }
 
-    // draws start board
+    // Метод для отрисовки начальной доски
     int start_draw()
     {
+        // Инициализация SDL2
         if (SDL_Init(SDL_INIT_EVERYTHING) != 0)
         {
             print_exception("SDL_Init can't init SDL2 lib");
-            return 1;
+            return 1; // Ошибка инициализации SDL
         }
+
+        // Если ширина и высота не заданы, устанавливаем их в зависимости от экрана
         if (W == 0 || H == 0)
         {
             SDL_DisplayMode dm;
             if (SDL_GetDesktopDisplayMode(0, &dm))
             {
-                print_exception("SDL_GetDesktopDisplayMode can't get desctop display mode");
+                print_exception("SDL_GetDesktopDisplayMode can't get desktop display mode");
                 return 1;
             }
+
+            // Выбираем минимальное значение между шириной и высотой экрана, чтобы доска была квадратной
             W = min(dm.w, dm.h);
-            W -= W / 15;
+            W -= W / 15; // Немного уменьшаем размер, чтобы оставить отступы
             H = W;
         }
+
+        // Создаем окно игры "Checkers"
         win = SDL_CreateWindow("Checkers", 0, H / 30, W, H, SDL_WINDOW_RESIZABLE);
         if (win == nullptr)
         {
             print_exception("SDL_CreateWindow can't create window");
-            return 1;
+            return 1; // Ошибка создания окна
         }
+
+        // Создаем рендерер для отрисовки графики
         ren = SDL_CreateRenderer(win, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
         if (ren == nullptr)
         {
             print_exception("SDL_CreateRenderer can't create renderer");
-            return 1;
+            return 1; // Ошибка создания рендерера
         }
+
+        // Загружаем текстуры для доски, шашек и кнопок
         board = IMG_LoadTexture(ren, board_path.c_str());
         w_piece = IMG_LoadTexture(ren, piece_white_path.c_str());
         b_piece = IMG_LoadTexture(ren, piece_black_path.c_str());
@@ -63,155 +76,180 @@ public:
         b_queen = IMG_LoadTexture(ren, queen_black_path.c_str());
         back = IMG_LoadTexture(ren, back_path.c_str());
         replay = IMG_LoadTexture(ren, replay_path.c_str());
+
+        // Проверяем, удалось ли загрузить все текстуры
         if (!board || !w_piece || !b_piece || !w_queen || !b_queen || !back || !replay)
         {
             print_exception("IMG_LoadTexture can't load main textures from " + textures_path);
-            return 1;
+            return 1; // Ошибка загрузки текстур
         }
+
+        // Получаем окончательный размер окна
         SDL_GetRendererOutputSize(ren, &W, &H);
+
+        // Создаем начальную матрицу игрового поля
         make_start_mtx();
+
+        // Перерисовываем игровую сцену
         rerender();
-        return 0;
+
+        return 0; // Успешный запуск
     }
 
     void redraw()
     {
-        game_results = -1;
-        history_mtx.clear();
-        history_beat_series.clear();
-        make_start_mtx();
-        clear_active();
-        clear_highlight();
+        game_results = -1; // Сбрасываем результаты игры (например, проигрыш или победа)
+        history_mtx.clear(); // Очищаем историю состояния доски
+        history_beat_series.clear(); // Очищаем историю серий побежденных фигур
+        make_start_mtx(); // Пересоздаем начальное состояние доски
+        clear_active(); // Убираем выделение активной клетки
+        clear_highlight(); // Убираем все выделенные клетки
     }
 
     void move_piece(move_pos turn, const int beat_series = 0)
     {
+        // Если фигура была побита (в начале хранящаяся в xb, yb), то ее нужно убрать с доски
         if (turn.xb != -1)
         {
-            mtx[turn.xb][turn.yb] = 0;
+            mtx[turn.xb][turn.yb] = 0; // Убираем побитую фигуру с позиции
         }
+
+        // Перемещаем фигуру с одной клетки на другую (используя другую версию функции)
         move_piece(turn.x, turn.y, turn.x2, turn.y2, beat_series);
     }
 
     void move_piece(const POS_T i, const POS_T j, const POS_T i2, const POS_T j2, const int beat_series = 0)
     {
+        // Проверяем, пуста ли целевая клетка
         if (mtx[i2][j2])
         {
             throw runtime_error("final position is not empty, can't move");
         }
+
+        // Проверяем, есть ли фигура на начальной клетке
         if (!mtx[i][j])
         {
             throw runtime_error("begin position is empty, can't move");
         }
+
+        // Если фигура дошла до последнего ряда (в зависимости от ее цвета), она становится дамкой
         if ((mtx[i][j] == 1 && i2 == 0) || (mtx[i][j] == 2 && i2 == 7))
-            mtx[i][j] += 2;
+            mtx[i][j] += 2; // Превращаем в дамку (прибавляем 2 к значению фигуры)
+
+        // Перемещаем фигуру на целевую позицию
         mtx[i2][j2] = mtx[i][j];
-        drop_piece(i, j);
-        add_history(beat_series);
+        drop_piece(i, j); // Убираем фигуру с начальной клетки
+        add_history(beat_series); // Добавляем ход в историю (с учетом серии побежденных фигур)
     }
 
     void drop_piece(const POS_T i, const POS_T j)
     {
-        mtx[i][j] = 0;
-        rerender();
+        mtx[i][j] = 0; // Убираем фигуру с клетки (ставим 0, означающее пустую клетку)
+        rerender(); // Перерисовываем доску, чтобы изменения отобразились на экране
     }
 
     void turn_into_queen(const POS_T i, const POS_T j)
     {
+        // Проверка, что фигура существует и она не является дамкой
         if (mtx[i][j] == 0 || mtx[i][j] > 2)
         {
-            throw runtime_error("can't turn into queen in this position");
+            throw runtime_error("can't turn into queen in this position"); // Выбрасываем исключение, если клетка пуста или уже дамка
         }
-        mtx[i][j] += 2;
-        rerender();
+        mtx[i][j] += 2; // Превращаем фигуру в дамку, прибавляя 2 (например, 1 -> 3 для белой дамки, 2 -> 4 для черной дамки)
+        rerender(); // Перерисовываем доску, чтобы изменения отобразились на экране
     }
     vector<vector<POS_T>> get_board() const
     {
-        return mtx;
+        return mtx; // Возвращаем текущее состояние доски (матрицу клеток)
     }
 
     void highlight_cells(vector<pair<POS_T, POS_T>> cells)
     {
+        // Проходим по всем переданным клеткам и помечаем их как подсвеченные
         for (auto pos : cells)
         {
             POS_T x = pos.first, y = pos.second;
-            is_highlighted_[x][y] = 1;
+            is_highlighted_[x][y] = 1; // Устанавливаем флаг подсветки для каждой клетки
         }
-        rerender();
+        rerender(); // Перерисовываем доску, чтобы подсветка отобразилась на экране
     }
 
     void clear_highlight()
     {
+        // Очищаем все клетки от подсветки (устанавливаем флаг подсветки в 0)
         for (POS_T i = 0; i < 8; ++i)
         {
-            is_highlighted_[i].assign(8, 0);
+            is_highlighted_[i].assign(8, 0); // Присваиваем каждой клетке значение 0 (не подсвечивается)
         }
-        rerender();
+        rerender(); // Перерисовываем доску, чтобы очистить подсветку
     }
 
     void set_active(const POS_T x, const POS_T y)
     {
-        active_x = x;
+        active_x = x; // Устанавливаем активные координаты клетки
         active_y = y;
-        rerender();
+        rerender(); // Перерисовываем доску, чтобы активная клетка отображалась
     }
 
     void clear_active()
     {
-        active_x = -1;
+        active_x = -1; // Сбрасываем координаты активной клетки (устанавливаем их в -1, чтобы показать, что нет активной клетки)
         active_y = -1;
-        rerender();
+        rerender(); // Перерисовываем доску, чтобы удалить выделение активной клетки
     }
 
     bool is_highlighted(const POS_T x, const POS_T y)
     {
-        return is_highlighted_[x][y];
+        return is_highlighted_[x][y]; // Возвращает, подсвечена ли данная клетка
     }
 
     void rollback()
     {
+        // Получаем количество ударов (битых фигур) для отката
         auto beat_series = max(1, *(history_beat_series.rbegin()));
+        // Откатываем состояние доски, удаляя элементы из истории
         while (beat_series-- && history_mtx.size() > 1)
         {
-            history_mtx.pop_back();
-            history_beat_series.pop_back();
+            history_mtx.pop_back(); // Удаляем последний ход из истории
+            history_beat_series.pop_back(); // Удаляем количество ударов, связанное с этим ходом
         }
-        mtx = *(history_mtx.rbegin());
-        clear_highlight();
-        clear_active();
+        mtx = *(history_mtx.rbegin()); // Восстанавливаем доску из последнего состояния истории
+        clear_highlight(); // Очищаем подсветку
+        clear_active(); // Очищаем активную клетку
     }
 
     void show_final(const int res)
     {
-        game_results = res;
-        rerender();
+        game_results = res; // Сохраняем результат игры (результат может быть: победа, ничья или проигрыш)
+        rerender(); // Перерисовываем доску, чтобы отобразить результат игры на экране
     }
 
     // use if window size changed
     void reset_window_size()
     {
-        SDL_GetRendererOutputSize(ren, &W, &H);
-        rerender();
+        SDL_GetRendererOutputSize(ren, &W, &H); // Получаем новый размер окна после изменения
+        rerender(); // Перерисовываем доску с учетом нового размера окна
     }
 
     void quit()
     {
-        SDL_DestroyTexture(board);
-        SDL_DestroyTexture(w_piece);
-        SDL_DestroyTexture(b_piece);
-        SDL_DestroyTexture(w_queen);
-        SDL_DestroyTexture(b_queen);
-        SDL_DestroyTexture(back);
-        SDL_DestroyTexture(replay);
-        SDL_DestroyRenderer(ren);
-        SDL_DestroyWindow(win);
-        SDL_Quit();
+        // Освобождаем ресурсы, связанные с графическими объектами и окном
+        SDL_DestroyTexture(board); // Удаляем текстуру для фона доски
+        SDL_DestroyTexture(w_piece); // Удаляем текстуру для белых фигур
+        SDL_DestroyTexture(b_piece); // Удаляем текстуру для черных фигур
+        SDL_DestroyTexture(w_queen); // Удаляем текстуру для белых дам
+        SDL_DestroyTexture(b_queen); // Удаляем текстуру для черных дам
+        SDL_DestroyTexture(back); // Удаляем текстуру фона
+        SDL_DestroyTexture(replay); // Удаляем текстуру кнопки для воспроизведения
+        SDL_DestroyRenderer(ren); // Удаляем рендерер
+        SDL_DestroyWindow(win); // Удаляем окно
+        SDL_Quit(); // Завершаем работу с SDL
     }
 
     ~Board()
     {
-        if (win)
-            quit();
+        if (win) // Если окно существует
+            quit(); // Вызываем функцию для освобождения ресурсов перед завершением работы программы
     }
 
 private:
--- Models/Move.h
+++ Models/Move.h
@@ -3,25 +3,33 @@
 
 typedef int8_t POS_T;
 
+// Структура, представляющая ход в игре
 struct move_pos
 {
-    POS_T x, y;             // from
-    POS_T x2, y2;           // to
-    POS_T xb = -1, yb = -1; // beaten
+    POS_T x, y;             // Начальная позиция (откуда фигура ходит)
+    POS_T x2, y2;           // Конечная позиция (куда фигура ходит)
+    POS_T xb = -1, yb = -1; // Координаты съеденной фигуры (-1, если фигура не бьет)
 
-    move_pos(const POS_T x, const POS_T y, const POS_T x2, const POS_T y2) : x(x), y(y), x2(x2), y2(y2)
+    // Конструктор для обычного хода (без взятия)
+    move_pos(const POS_T x, const POS_T y, const POS_T x2, const POS_T y2)
+        : x(x), y(y), x2(x2), y2(y2)
     {
     }
+
+    // Конструктор для хода со взятием фигуры (указывается позиция побитой фигуры)
     move_pos(const POS_T x, const POS_T y, const POS_T x2, const POS_T y2, const POS_T xb, const POS_T yb)
         : x(x), y(y), x2(x2), y2(y2), xb(xb), yb(yb)
     {
     }
 
-    bool operator==(const move_pos &other) const
+    // Оператор сравнения (два хода считаются равными, если начальная и конечная позиции совпадают)
+    bool operator==(const move_pos& other) const
     {
         return (x == other.x && y == other.y && x2 == other.x2 && y2 == other.y2);
     }
-    bool operator!=(const move_pos &other) const
+
+    // Оператор неравенства (если ходы не равны)
+    bool operator!=(const move_pos& other) const
     {
         return !(*this == other);
     }
